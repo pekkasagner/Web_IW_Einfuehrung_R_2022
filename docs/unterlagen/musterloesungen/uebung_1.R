980219f2015cc240856996583137a3b54857e753
pekkasagner <pekka.sagner@yahoo.de>
2022-05-05 16:55:24 +0200

    Updated lecture 3

--- docs/unterlagen/musterloesungen/uebung_1.R
+++ docs/unterlagen/musterloesungen/uebung_1.R
@@ -1,109 +1,100 @@
-# Autor: Pekka Sagner
-# Zuletzt bearbeitet: 7.4.2022
-# Inhalt: Übungsblatt 1 - Musterlösung
+#Datum: 8.4.2022
+#Autor: Pekka Sagner
+#Übung 1: Erste Schritte mit R und R-Studio
+#----------------------------------------------------
 
-#Aufgabe 6 
-#b)
-1 + 2
-(1 + 2) * 3
-(1 + 2) * 3 - 6
-((1 + 2) * 3 - 6) / 4
-log(((1 + 2) * 3 - 6) / 4) 
+#Aufgabe 6: a/b)
+#i)
+ergebnis_1 <- 1+2
 
-#c)
+#ii)
+ergebnis_2 <- ergebnis_1*3
+#Alternative
+(1+2)*3
+
+#iii)
+ergebnis_3 <- ergebnis_2-6
+
+#iv)
+ergebnis_4 <- ergebnis_3/4
+
+#v)
+ergebnis_final <- log(ergebnis_4)
+
+#6c)
 vektor_1 <- c(2, 9, -4, 0)
-vektor_1
 
-#d)
-#Hilfsfunktion wird mit ? vor dem Funktionsnamen genutzt.
-#Beispiel:
+#6d)
 ?c()
-#weiteres Beispiel:
-?mean()
 
-#e)
-#Arithmetisches Mittel:
+#6e)
 mean_vektor_1 <- mean(vektor_1)
-#Minimum:
-min_vektor_1 <- min(vektor_1)
-#Maximum
-max_vektor_1 <- max(vektor_1)
-#Median
-median_vektor_1 <- median(vektor_1)
-#betrachten der Objekte:
 mean_vektor_1
+
+min_vektor_1 <- min(vektor_1)
 min_vektor_1
+
+max_vektor_1 <- max(vektor_1)
 max_vektor_1
+
+median_vektor_1 <- median(vektor_1)
 median_vektor_1
 
 #f)
 v_2 <- c(2, 9, NA, -4, 0)
-#Mittelwert von v_2 bestimmen:
-mean(v_2)
-#NA steht für Leere Werte in R.
-#Um dennoch den Durchschnitt zu bestimmen, nutzt man na.rm = TRUE
-?mean()
-#Funktion mit na.rm anwenden
+          
 mean(v_2, na.rm = TRUE)
+min(v_2, na.rm = T)
+max(v_2, na.rm = T)
+median(v_2, na.rm = T)
 
-
-#Aufgabe 7:
-#Erstellen der Skalare:
-vektor_drei <- 3
+#Aufgabe 7
+#a)
+vektor_3 <- 3
 vektor_pi <- pi
-vektor_euler <- exp(1)
-vektor_log10 <- log(10)
+(vektor_euler <- exp(1))
+vektor_log <- log(10)
 
-#Logischer Test, Beispiele:
-vektor_drei == vektor_pi
-vektor_euler > vektor_log10
-vektor_pi < vektor_log10
+#b)logische Tests durchführen
+vektor_log > vektor_euler
 
+vektor_pi < vektor_3
+
+vektor_euler > vektor_log & vektor_euler < vektor_pi
+
+vektor_pi > vektor_3
+
+vektor_pi != vektor_3
+
+vektor_log == vektor_3
 
 #Aufgabe 8
-#a) Verbinden der Skalare zu einem Vektor
-vektor_verbunden <- c(vektor_drei, vektor_pi, vektor_euler, vektor_log10)
-vektor_verbunden
-
-#b) Datentyp
-typeof(vektor_verbunden)
-
+#a)
+vektor_kombi <- c(vektor_3 , vektor_pi, vektor_euler, vektor_log)
+#b)
+typeof(vektor_kombi)
 #c)
-vektor_verbunden_char <- c(vektor_verbunden, "hallo")
-vektor_verbunden_char
-#typ:
-typeof(vektor_verbunden_char)
+vektor_kombi_skalar <- c(vektor_kombi, "hallo")
+typeof(vektor_kombi_skalar)
+vektor_kombi_skalar <- as.numeric(vektor_kombi_skalar)
 
 #d)
-#double:
-double_vector <- c(4.3, 2.8, 4, 5.3)
-#character:
-char_vector <- c("hallo", "schlechtes", "wetter", "heute")
-#logical:
-log_vector <- c(TRUE, FALSE, T, F)
+double_vektor <- c(5, 1.2, 30, 9.5)
+character_vektor <- c("hallo", "tschüss", "hochschule", "april")
+logical_vektor <- c(TRUE, FALSE, F, T)
 
-#prüfen Datentypen:
-typeof(log_vector)
+length(double_vektor)
+#Datentyp prüfen
+typeof(double_vektor)
+typeof(character_vektor)
+typeof(logical_vektor)
 
-#e) 
-factor_vector <- factor(char_vector)
-factor_vector
-
-#mit typeof prüfen:
-typeof(factor_vector)
-
-#Klasse prüfen:
-class(factor_vector)
+#e)
+character_vektor_as_factor <- factor(character_vektor) 
+typeof(character_vektor_as_factor)
+class(character_vektor_as_factor)
 
 #f)
-factor_gender <- factor(c("diverse", "female", "male"))
-factor_gender
+gender_vektor <- factor(c("diverse", "female", "male"))
 
-#Levels Reihenfolge ändern
-factor_gender_reordered <- factor(factor_gender, 
-                                  levels = c("female", "male", "diverse"))
-factor_gender_reordered
-
-
-
-
+gender_vektor_rearranged <- factor(gender_vektor, levels = c("female", "male", "diverse"))
